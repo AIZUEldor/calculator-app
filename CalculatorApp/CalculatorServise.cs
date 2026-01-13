17d9398c28c575d3a6989c507666b8a60d36ee7c
AIZUEldor <eldor9406@icloud.com>
2026-01-13 14:41:36 +0500

    Add project files.

---
+++ CalculatorApp/CalculatorServise.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class CalculatorServise
+    {
+        public void HisoblashLogig()
+        {
+            Console.WriteLine("Iltimos hisoblamoqchi bo'lgan raqamlaringizni kiriting : ");
+            Console.Write(" Ifodani kiriting  =  ");
+            string summ = Console.ReadLine();
+
+            int summa = 0;
+
+            char currentSign = '+'; // Dastlabki amal +
+
+            foreach (char c in summ)
+            {
+                if (c == ' ')
+                {
+                    continue; // Probelni o'tkazib yuborish
+                }
+                if (c == '+' || c == '-')
+                {
+                    currentSign = c; // Joriy amalni yangilash
+                }
+                else if (char.IsDigit(c))
+                {
+                    int number = int.Parse(c.ToString());
+                    if (currentSign == '+')
+                    {
+                        summa += number;
+                    }
+                    else if (currentSign == '-')
+                    {
+                        summa -= number;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(" Xato kiritdingiz !!!!! \n Iltimos, bir xonalik sonlardan iborat qo'shish va ayirish misolini yozing !!!");
+                    return;
+                }
+
+            }
+            Console.WriteLine("Natija : " + summa);
+        }
+    }
+}
+
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Timers;
+
+namespace CalculatorApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorServise calculator = new CalculatorServise();
+            Console.WriteLine("Heloo !!! . Cankulyator dasturiga xush kelipsiz ! Boshlaymizmi ? ");
+            Console.Write("  Ha \n. Yo'q .... ");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "ha")
+            { 
+              bool continueCalculation = true;
+                while (continueCalculation)
+                {
+                    calculator.HisoblashLogig();
+                    Console.WriteLine("Yana hisoblamoqchimisiz? (Ha/Yo'q)");
+                    string continueAnswer = Console.ReadLine();
+                    if (continueAnswer.ToLower() != "ha")
+                    {
+                        continueCalculation = false;
+                    }
+                    
+                }
+                Console.WriteLine("Dasturdan foydalanganingiz uchun rahmat ! Xayr salomat bo'ling :) ");
+
+            }
+            else
+            {
+                Console.WriteLine("Dasturdan foydalanganingiz uchun rahmat ! Xayr :)");
+            }
+        }
+
+       
+    }
+}
+//    INPUT:     2+5-1
+//OUTPUT:  6
+
+//Shartlar:
+//1. Faqat + va - amallari bajariladi
+//2. Faqat 1 xonalik sonlar kiritiladi
+//3. Foydalanuvchi doimo to'g'ri qiymatlarni kiritishi ta'minlanadi
+
+//Kuchililar uchun:
+//1. orada probel tashlab ketilgan holatlarni ham inobatga olish kerak, Ya'ni:
+//INPUT:     2 + 5 - 1
+//OUTPUT:  6
+//2. Foydaluvchi xato qiymatlarni ham kiritib qoyishi mumkin.
+
+//INPUT:  a&+51 -01
+//OUTPUT: Iltimos, bir xonalik sonlardan iborat qo'shish va ayirish misolini yozing
+
+    
+
